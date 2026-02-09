7e42cf9ea63e846298de9bab0188d54081657e55
Kirk Saunders <torikirksaunders@gmail.com>
2026-02-08 19:55:19 -0500

    adding changes to get some help

---
+++ js/index.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+
+    {
+        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+        author: "Eric Schmidt",
+    }
+]
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-app.use(express.static("./public"));
+app.use(express.static("./public/"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.listen(port, () => {
